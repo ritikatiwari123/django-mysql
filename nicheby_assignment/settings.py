d7190b13213dc9052177988e3e1fa4a29b3700f0
ritika <ritika.tiwari@otocapital.in>
2023-07-26 13:11:17 +0530

    configure django with mysql db../

--- nicheby_assignment/settings.py
+++ nicheby_assignment/settings.py
@@ -77,9 +77,16 @@ WSGI_APPLICATION = 'nicheby_assignment.wsgi.application'
 
 DATABASES = {
      'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+         'ENGINE': 'django.db.backends.mysql',
+         'NAME': 'nichenydb',
+         'USER': 'nicheby',
+         'PASSWORD': 'Nicheby1',
+         'HOST': 'nichebydb.cs81agayomvj.ap-northeast-1.rds.amazonaws.com',
+         'PORT': '3306'
+
+     }
 }
 
 
